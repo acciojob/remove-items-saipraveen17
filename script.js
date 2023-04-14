32359a2a1a9769710bd6740f4ec651c64631456d
Praveen Katikireddy <praveenk9417@gmail.com>
2023-04-14 16:07:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+// get reference to the button element
+const removeBtn = document.querySelector('input[type="button"]');
+
+// add click event listener to the button
+removeBtn.addEventListener('click', () => {
+  // get reference to the select element
+  const selectElem = document.getElementById('colorSelect');
+  
+  // get the currently selected option
+  const selectedOption = selectElem.options[selectElem.selectedIndex];
+  
+  // remove the selected option from the list
+  selectElem.removeChild(selectedOption);
+});
